49ae3442f2abd2800cb5ed3474265e7d4eb37798
Squirrel <sqmir@163.com>
2018-04-08 14:49:34 +0800

    chain base message, fix egenesis

---
+++ plugins/blockchain_plugin/include/blockbuilder_plugin.hpp
@@ -0,0 +1,33 @@
+/**
+ *  @file
+ *  @copyright defined in xmax/LICENSE
+ */
+#pragma once
+#include <application.hpp>
+#include <plugin.hpp>
+#include <boost/container/flat_set.hpp>
+#include <chain_xmax.hpp>
+#include <chaindata_plugin.hpp>
+using namespace Baseapp;
+namespace Xmaxplatform {
+
+
+
+class blockchain_plugin : public plugin<blockchain_plugin> {
+public:
+
+    BASEAPP_DEPEND_PLUGINS((chaindata_plugin))
+    blockchain_plugin();
+   virtual ~blockchain_plugin();
+
+   virtual void set_program_options(options_description& cli, options_description& cfg) override;
+
+   void plugin_initialize(const variables_map& options);
+   void plugin_startup();
+   void plugin_shutdown();
+
+private:
+    std::unique_ptr<class chain_plugin_impl> my;
+};
+
+}
