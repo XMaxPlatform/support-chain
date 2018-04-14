6cd59abdcf742b453a5f01f6229b7169b9894fe1
guozhenghao <williamsg@163.com>
2018-04-14 15:23:47 +0800

    Adjust cmake config in windows, enable changing LLVM and OpenSSL path

--- CMakeModules/windows_config.cmake
+++ CMakeModules/windows_config.cmake
@@ -1,15 +1,31 @@
 if(WIN32)
 	message(STATUS "-------------- Window config --------------")
 	
-	set(LLVM_DIR "${XMAX_ROOT_DIR}/wasm-compiler/llvm/build/lib/cmake/llvm")
+	
+	if(NOT DEFINED XMAX_ROOT_DIR)
+		set(XMAX_ROOT_DIR ${CMAKE_SOURCE_DIR})
+	endif()
+	message(STATUS "XMAX_ROOT_DIR: ${XMAX_ROOT_DIR}")
+	
+	if(NOT DEFINED LLVM_DIR)
+		message(STATUS "Not defined LLVM_DIR")
+		set(LLVM_DIR "${XMAX_ROOT_DIR}/wasm-compiler/llvm/build/lib/cmake/llvm")
+	endif()
 	message(STATUS "LLVM_DIR: ${LLVM_DIR}")
 
-	set(OPENSSL_ROOT_DIR "${XMAX_ROOT_DIR}/libraries/OpenSSL-Win64")
+
+	if(NOT DEFINED OPENSSL_ROOT_DIR)
+		set(OPENSSL_ROOT_DIR "${XMAX_ROOT_DIR}/libraries/OpenSSL-Win64")
+	endif()
+	message(STATUS "OPENSSL_ROOT_DIR: ${OPENSSL_ROOT_DIR}")
+	
 	set(OPENSSL_INCLUDE_DIR ${OPENSSL_ROOT_DIR}/include)
 	set(OPENSSL_LIBRARY_DIR ${OPENSSL_ROOT_DIR}/lib)
-	message(STATUS "OPENSSL_ROOT_DIR: ${OPENSSL_ROOT_DIR}")
 
-	set(Secp256k1_ROOT_DIR "${XMAX_ROOT_DIR}/libraries/secp256k1-zkp")
+
+	if(NOT DEFINED Secp256k1_ROOT_DIR)
+		set(Secp256k1_ROOT_DIR "${XMAX_ROOT_DIR}/libraries/secp256k1-zkp")
+	endif()	
 	set(Secp256k1_LIBRARY "${Secp256k1_ROOT_DIR}/lib")
 	set(Secp256k1_INCLUDE_DIR "${Secp256k1_ROOT_DIR}/include")
 	message(STATUS "Secp256k1_ROOT_DIR: ${Secp256k1_ROOT_DIR}")
