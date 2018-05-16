8673209e278c120ee1f4f85baab9eeca83cfb3b8
williamsg <williamsgzh85@gmail.com>
2018-05-16 19:36:20 +0800

    Add test contract

---
+++ contract_code/testcontract/testcontract.cpp
@@ -0,0 +1,59 @@
+#include <testcontract.hpp>
+
+const uint64_t max_quantity = 9000000000ull;
+
+namespace testcontract {
+
+    void apply_testaction( const testcontract::testaction& msg) {
+        if(msg.quantity <= 0 || msg.quantity > max_quantity) {
+            return;
+        }
+
+         testdata data(msg.account);         
+
+         if(!testdatas::get(data, N(msg.account))) {
+             testdatas::store(testdata(msg.account, msg.quantity), XNAME(msg.account));
+             return;
+         }
+         else {
+             if(data.quantity > max_quantity - msg.quantity) {
+                 return;
+             }
+
+             data.quantity += msg.quantity;
+             testdatas::store(data, XNAME(msg.account));
+         }
+    }
+
+
+}
+
+
+using namespace testcontract;
+
+/**
+ *  The init() and apply() methods must have C calling convention so that the blockchain can lookup and
+ *  call these methods.
+ */
+extern "C" {
+     /**
+     *  This method is called once when the contract is published or updated.
+     */
+    void init()  {        
+        testdata data(XNAME(testcontract));
+        if ( !testdatas::get(data, XNAME(testcontract) ))
+            testdatas::store(testdata(1000000000ull), XNAME(testcontract));
+    }
+
+    /// The apply method implements the dispatch of events to this contract
+    void apply( uint64_t code, uint64_t action ) {
+        //Dispatch call method accroding to the code and action
+        if( code == XNAME(testcontract)) {
+            if( action == XNAME(testaction)) {
+                apply_testaction( current_message<testcontract::testaction>() );
+            }
+
+
+        }
+    }
+}
---
+++ contract_code/testcontract/testcontract.hpp
@@ -0,0 +1,44 @@
+#include <xmaxlib/xmax.hpp>
+#include <xmaxlib/core.h>
+#include <xmaxlib/database.hpp>
+
+namespace testcontract
+{
+typedef Xmaxplatform::token<uint64_t, XNAME(testcontract)> test_tokens;
+
+/**
+     * @brief test action
+     * @abi action msg
+     */
+struct testaction
+{
+    /**
+     *  test account field
+     */
+    account_name account;
+
+    /**
+     *  account to transfer to
+     */
+    test_tokens quantity;
+}
+
+/**
+     * @brief Test data for store data
+     * @abi table testdata
+     */
+struct testdata
+{
+    uint64_t key;
+
+    test_tokens balance;
+
+    testdata(uint64_t key = XNAME(testcontract), test_tokens b = test_tokens()):key(key), balance(b) {}
+}
+
+/**
+   Defines the database table for test information
+   **/
+using testdatas = Xmaxplatform::table<XNAME(defaultscope), XNAME(testdata), XNAME(testdata), testdata, uint64_t>;
+
+} // namespace testcontract
