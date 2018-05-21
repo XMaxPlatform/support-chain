b4f9f50cbb659b0f551beeacfade58d30e898312
赵航 <hangzhao@126.com>
2018-05-21 21:11:20 +0800

    Add js vm code.

---
+++ blockchain/chain/include/jsvm_xmax.h
@@ -0,0 +1,94 @@
+#pragma once
+#pragma once
+#include <blockchain_exceptions.hpp>
+#include <message_xmax.hpp>
+#include <message_context_xmax.hpp>
+
+#include <libplatform/libplatform.h>
+#include <v8.h>
+
+
+using namespace v8;
+
+namespace Xmaxplatform {
+	namespace Chain {
+		class jsvm_xmax {
+		public:
+			enum key_type {
+				str,
+				i64,
+				i128i128,
+				i64i64i64,
+				invalid_key_type
+			};
+			typedef map<name, key_type> TableMap;
+			struct ModuleState {
+				/*
+				Runtime::ModuleInstance* instance = nullptr;
+				IR::Module*              module = nullptr;
+				int                      mem_start = 0;
+				int                      mem_end = 1 << 16;
+				vector<char>             init_memory;*/
+
+				Local<Script>			 current_script;
+				Isolate*                 current_isolate;
+				Local<Context>           current_context;
+
+				Isolate::Scope*          current_isolate_scope;
+				HandleScope*             current_handle_scope;	
+				Context::Scope*			 current_context_scope;
+
+				fc::sha256               code_version;
+				TableMap                 table_key_types;
+				bool                     tables_fixed = false;
+			};
+
+			static jsvm_xmax& get();
+
+			void init(message_context_xmax& c);
+			void apply(message_context_xmax& c, uint32_t execution_time, bool received_block);
+			void validate(message_context_xmax& c);
+			void precondition(message_context_xmax& c);
+
+			int64_t current_execution_time();
+
+			void checktime();
+
+			static key_type to_key_type(const Basetypes::type_name& type_name);
+			static std::string to_type_name(key_type key_type);
+
+			message_context_xmax*       current_message_context = nullptr;
+			message_context_xmax*       current_validate_context = nullptr;
+			message_context_xmax*       current_precondition_context = nullptr;
+
+// 			Runtime::MemoryInstance*   current_memory = nullptr;
+// 			Runtime::ModuleInstance*   current_module = nullptr;
+// 			wasm_memory*               current_memory_management = nullptr;
+			ModuleState*               current_state = nullptr;
+			TableMap*                  table_key_types = nullptr;
+			bool                       tables_fixed = false;
+			int64_t                    table_storage = 0;
+
+			uint32_t                   checktime_limit = 0;
+
+			int32_t                    per_code_account_max_db_limit_mbytes = Config::default_per_code_account_max_db_limit_mbytes;
+			uint32_t                   row_overhead_db_limit_bytes = Config::default_row_overhead_db_limit_bytes;
+
+		private:
+			void InitV8();
+			void load(const account_name& name, const Basechain::database& db);
+			void  vm_validate();
+			void  vm_precondition();
+			void  vm_apply();
+			void  vm_onInit();
+			//U32   vm_pointer_to_offset(char*);
+
+
+
+			map<account_name, ModuleState> instances;
+			fc::time_point checktimeStart;
+
+			jsvm_xmax();
+		};
+	}
+}
---
+++ blockchain/chain/jsvm_xmax.cpp
@@ -0,0 +1,220 @@
+#include "jsvm_xmax.h"
+
+/**
+*  @file
+*  @copyright defined in xmax/LICENSE
+*/
+#include <boost/function.hpp>
+#include <boost/multiprecision/cpp_bin_float.hpp>
+#include <jsvm_xmax.h>
+#include <chain_xmax.hpp>
+#include <vm_native_interface.hpp>
+#include <objects/key_value_object.hpp>
+#include <objects/account_object.hpp>
+#include <objects/xmx_token_object.hpp>
+#include <abi_serializer.hpp>
+#include <chrono>
+#include <boost/lexical_cast.hpp>
+#include <fc/utf8.hpp>
+#if WIN32
+#include <fc/int128.hpp>
+#endif
+
+
+namespace Xmaxplatform {
+
+	namespace Chain {
+
+		jsvm_xmax::jsvm_xmax() {
+
+		}
+
+		jsvm_xmax::key_type jsvm_xmax::to_key_type(const Basetypes::type_name& type_name)
+		{
+			if ("str" == type_name)
+				return str;
+			if ("i64" == type_name)
+				return i64;
+			if ("i128i128" == type_name)
+				return i128i128;
+			if ("i64i64i64" == type_name)
+				return i64i64i64;
+
+			return invalid_key_type;
+		}
+
+		std::string jsvm_xmax::to_type_name(key_type key_type)
+		{
+			switch (key_type)
+			{
+			case str:
+				return "str";
+			case i64:
+				return "i64";
+			case i128i128:
+				return "i128i128";
+			case i64i64i64:
+				return "i64i64i64";
+			default:
+				return std::string("<invalid key type - ") + boost::lexical_cast<std::string>(int(key_type)) + ">";
+			}
+		}
+
+		jsvm_xmax& jsvm_xmax::get() {
+			static jsvm_xmax*  jsvm = nullptr;
+			if (!jsvm)
+			{
+				//
+
+				jsvm = new jsvm_xmax();
+				jsvm->InitV8();
+			}
+			return *jsvm;
+	  }
+
+#ifdef NDEBUG
+		const int CHECKTIME_LIMIT = 3000;
+#else
+		const int CHECKTIME_LIMIT = 36000;
+#endif
+
+		void jsvm_xmax::init(message_context_xmax& c)
+		{
+			try {
+				current_validate_context = &c;
+				current_precondition_context = &c;
+				current_message_context = &c;
+				checktime_limit = CHECKTIME_LIMIT;
+
+				load(c.code, c.db);
+
+				vm_onInit();
+
+			} FC_CAPTURE_AND_RETHROW()
+		}
+
+		void jsvm_xmax::InitV8()
+		{
+			V8::InitializeICUDefaultLocation("");
+			V8::InitializeExternalStartupData("");
+			v8::Platform* platform = platform::CreateDefaultPlatform();
+			V8::InitializePlatform(platform);
+			V8::Initialize();
+		}
+
+		void  jsvm_xmax::vm_apply() {
+		
+			Handle<String> js_func_name = String::NewFromUtf8(current_state->current_isolate, "init", NewStringType::kNormal).ToLocalChecked();
+			Handle<v8::Value>  js_func_val = current_state->current_context->Global()->Get(js_func_name);
+			if (!js_func_val->IsFunction())
+			{
+				std::cerr << "Can't find js funcion init()" << std::endl;
+			}
+			Handle<Function> js_func = Handle<Function>::Cast(js_func_val);
+
+			Handle<v8::Value> hResult = js_func->Call(current_state->current_context->Global(), 0, nullptr);
+		
+		}
+
+		void  jsvm_xmax::vm_onInit()
+		{
+			try {
+				try {
+					//const auto& recipient = db.get<account_object, by_name>(name);
+
+					HandleScope current_handle_scope(current_state->current_isolate);
+					Local<Context> context = Context::New(current_state->current_isolate);
+					
+
+					// Enter the context for compiling and running the hello world script.
+					//Context::Scope context_scope(context);
+					state.current_context_scope = new Context::Scope(context);
+					const char* jscode = recipient.code.data();
+
+					// Create a string containing the JavaScript source code.
+					Local<String> source =
+						String::NewFromUtf8(isolate, jscode,
+							NewStringType::kNormal).ToLocalChecked();
+
+					// Compile the source code.
+					Local<Script> script = Script::Compile(context, source).ToLocalChecked();
+					state.current_script = script;
+
+				
+					Handle<String> js_func_name = String::NewFromUtf8(current_state->current_isolate , "init", NewStringType::kNormal).ToLocalChecked();
+					Handle<v8::Value>  js_func_val = current_state->current_context->Global()->Get(js_func_name);
+					if (!js_func_val->IsFunction())
+					{
+						std::cerr << "Can't find js funcion init()" << std::endl;
+					}
+					Handle<Function> js_func = Handle<Function>::Cast(js_func_val);
+					Handle<v8::Value> hResult = js_func->Call(current_state->current_context->Global(), 0, nullptr);
+				}
+				catch (const Runtime::Exception& e) {
+					edump((std::string(describeExceptionCause(e.cause))));
+					edump((e.callStack));
+					throw;
+				}
+			} FC_CAPTURE_AND_RETHROW()
+		}
+
+		void jsvm_xmax::load(const account_name& name, const Basechain::database& db)
+		{
+			const auto& recipient = db.get<account_object, by_name>(name);
+			
+
+			auto& state = instances[name];
+			if (state.code_version != recipient.code_version) {
+ 				
+ 				state.code_version = fc::sha256();
+				state.table_key_types.clear();
+
+				try
+				{
+					const auto init_time = fc::time_point::now();
+
+					Isolate::CreateParams create_params;
+					create_params.array_buffer_allocator =
+						v8::ArrayBuffer::Allocator::NewDefaultAllocator();
+					Isolate* isolate = Isolate::New(create_params);
+					state.current_isolate = isolate;
+					state.current_isolate_scope = new Isolate::Scope(isolate);
+
+
+
+					//init abi
+					Basetypes::abi abi;
+					if (Basetypes::abi_serializer::to_abi(recipient.abi, abi))
+					{
+						state.tables_fixed = true;
+						for (auto& table : abi.tables)
+						{
+							const auto key_type = to_key_type(table.index_type);
+							if (key_type == invalid_key_type)
+								throw Serialization::FatalSerializationException("For code \"" + (std::string)name + "\" index_type of \"" +
+									table.index_type + "\" referenced but not supported");
+
+							state.table_key_types.emplace(std::make_pair(table.table_name, key_type));
+						}
+					}
+				}
+				catch (Serialization::FatalSerializationException exception)
+				{
+					std::cerr << "Error deserializing WebAssembly binary file:" << std::endl;
+					std::cerr << exception.message << std::endl;
+					throw;
+				}
+				//current_module = state.instance;
+				//current_memory = getDefaultMemory(current_module);
+				current_state = &state;
+				table_key_types = &state.table_key_types;
+				tables_fixed = state.tables_fixed;
+				table_storage = 0;
+			}
+		}
+
+	}
+}
+
+
+
