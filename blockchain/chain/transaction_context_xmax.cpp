4009bf964d8c209f47e25fe1f18047afb242db76
fengzi <liuxingzdh@163.com>
2018-06-28 18:51:04 +0800

    transaction context init.

--- blockchain/chain/transaction_context_xmax.cpp
+++ blockchain/chain/transaction_context_xmax.cpp
@@ -7,6 +7,12 @@
 namespace Xmaxplatform {
 namespace Chain {
 
+	transaction_context_xmax::transaction_context_xmax(chain_xmax& _chain, const signed_transaction& _trx, fc::time_point _start /* = fc::time_point::now() */)
+		: chain(_chain)
+		, trx(_trx)
+		, start_time(_start)
+	{
 
+	}
 }
 }
