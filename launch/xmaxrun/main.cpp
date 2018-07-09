8fccb3954fc25aa5cbd06d05bf41f065e823c936
赵航 <hangzhao@126.com>
2018-07-09 15:12:24 +0800

    Fix v8 exit crash error.

--- launch/xmaxrun/main.cpp
+++ launch/xmaxrun/main.cpp
@@ -59,14 +59,12 @@ int main(int argc, char** argv)
 
 #ifdef USE_V8
 		Xmaxplatform::Chain::jsvm_xmax::get().V8EnvInit();
-		v8::Isolate::Scope isolate_scope(Xmaxplatform::Chain::jsvm_xmax::get().V8GetIsolate());
-
-		// Create a stack-allocated handle scope.
-		v8::HandleScope handle_scope(Xmaxplatform::Chain::jsvm_xmax::get().V8GetIsolate());
 		{
+			v8::Isolate::Scope isolate_scope(Xmaxplatform::Chain::jsvm_xmax::get().V8GetIsolate());
+			// Create a stack-allocated handle scope.
+			v8::HandleScope handle_scope(Xmaxplatform::Chain::jsvm_xmax::get().V8GetIsolate());
 			v8::Local<v8::ObjectTemplate> global = v8::ObjectTemplate::New(Xmaxplatform::Chain::jsvm_xmax::get().V8GetIsolate());
 			Xmaxplatform::Chain::jsvm_xmax::get().V8SetupGlobalObjTemplate(&global);
-
 #endif
 
 			if (!app().init<Xmaxplatform::blockchain_plugin>(argc, argv))
