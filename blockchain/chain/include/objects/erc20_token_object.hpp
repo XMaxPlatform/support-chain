6e597744a0a8be87dc5db75169857beb5a68f428
williamsg <williamsg@163.com>
2018-07-12 12:28:58 +0800

    Modify the erc20 object fields to adapt the new design

--- blockchain/chain/include/objects/erc20_token_object.hpp
+++ blockchain/chain/include/objects/erc20_token_object.hpp
@@ -21,11 +21,11 @@ namespace Xmaxplatform {
 			id_type id;
 			Basetypes::asset_symbol token_name;
 			Basetypes::account_name owner_name;
-			Basetypes::share_type token_amount = 0;
+			Basetypes::share_type balance = 0;
 			Basetypes::share_type total_supply = 0;
 		};
 		
-		struct by_token_and_owner;
+		struct by_token_name;
 
 		using erc20_token_multi_index = Basechain::shared_multi_index_container<
 			erc20_token_object,
@@ -33,12 +33,8 @@ namespace Xmaxplatform {
 			ordered_unique<tag<by_id>,
 				member<erc20_token_object, erc20_token_object::id_type, &erc20_token_object::id>
 			>,
-			ordered_unique<tag<by_token_and_owner>,
-				composite_key<
-				erc20_token_object,
-				member<erc20_token_object, Basetypes::asset_symbol, &erc20_token_object::token_name>,
-				member<erc20_token_object, Basetypes::account_name, &erc20_token_object::owner_name>
-				>
+			ordered_unique<tag<by_token_name>,
+				member<erc20_token_object, Basetypes::asset_symbol, &erc20_token_object::token_name>
 			>
 			>
 		>;
@@ -49,12 +45,8 @@ namespace Xmaxplatform {
 			ordered_unique<tag<by_id>,
 			member<erc20_token_object, erc20_token_object::id_type, &erc20_token_object::id>
 			>,
-			ordered_unique<tag<by_token_and_owner>,
-				composite_key<
-				erc20_token_object,
-				member<erc20_token_object, Basetypes::asset_symbol, &erc20_token_object::token_name>,
-				member<erc20_token_object, Basetypes::account_name, &erc20_token_object::owner_name>
-			>
+			ordered_unique<tag<by_token_name>,
+			member<erc20_token_object, Basetypes::asset_symbol, &erc20_token_object::token_name>
 			>
 			>
 		>;
