b021a770a3d831e801806d86b946cdd1380bfc90
williamsg <williamsg@163.com>
2018-07-20 15:55:43 +0800

    Implement the u128 name encode and decode logic

--- foundation/basetypes/include/longname.hpp
+++ foundation/basetypes/include/longname.hpp
@@ -4,6 +4,7 @@
 */
 #pragma once
 #include <basedef.hpp>
+#include <unordered_map>
 #include <string>
 
 #if WIN32
@@ -25,6 +26,8 @@ namespace LongCode{
 	{		
 	public:		
 		typedef NameCodeType name_code_type;
+		typedef size_t size_type;
+		typedef int8_t char_code_type;
 
 		long_name() : namecode_() {}
 		
@@ -88,43 +91,129 @@ namespace LongCode{
 
 		operator bool()const { return namecode_ != 0; }
 		operator NameCodeType()const { return namecode_; }
+						
+
+		static inline constexpr size_type char_size() {
+			static_assert(false, "Need implemented in the specific class.");
+		}
+
+		static inline constexpr size_type type_size() {
+			return sizeof(NameCodeType);
+		}
+
+		static inline constexpr size_type type_bits() {
+			return type_size() * 8;
+		}
+
+		static inline constexpr size_type max_char_count() {
+			return type_size() / char_size();
+		}
+
+		static inline constexpr char_code_type char_code_mask() {
+			return static_cast<char_code_type>((1 << char_size()) - 1);
+		}
+
+		static inline constexpr char_code_type last_code_mask() {
+			return static_cast<char_code_type>((1 << (type_bits() - max_char_count() * char_size())) - 1);
+		}
 		
-		static inline std::string to_string(NameCodeType name) {
-			static_assert(false, "Not implemented yet.");
-			return "";
+		static inline constexpr char_code_type invalid_code() {
+			return 0;
+		}
+
+		static char_code_type char_to_code(char chr) {
+			static_assert(false, "Need implemented in the specific class.");
+		}
+
+		static char code_to_char(char_code_type code) {
+			static_assert(false, "Need implemented in the specific class.");
+		}
+
+		static inline std::string to_string(NameCodeType name_code) {
+			if (name_code == 0)
+				return "";
+
+			NameCodeType tmp = name_code;
+			std::string res = "";
+
+			for (size_type i = 0; i < max_char_count(); ++i)
+			{
+				char_code_type code = (char)((tmp >> (type_bits() - char_size() * (i + 1))) & char_code_mask());
+				if (code == invalid_code())
+				{
+					break;
+				}
+				res += code_to_char(code);
+			}
+
+			return std::move(res);
 		}
 
 		static inline NameCodeType to_name_code(const char* str) {
-			static_assert(false, "Not implemented yet.");
-			return NameCodeType{};
-		}		
+			size_type len = 0;
+			while (str[len]) ++len;
+
+			NameCodeType value = invalid_code();
+
+			for (size_type i = 0; i <= max_char_count(); ++i) {
+				NameCodeType c = 0;
+				if (i < len && i <= max_char_count()) c = NameCodeType(char_to_code(str[i]));
+
+				if (i < max_char_count()) {
+					c &= char_code_mask();
+					c <<= type_bits() - char_size() * (i + 1);
+				}
+				else {
+					c &= last_code_mask();
+				}
+
+				value |= c;
+			}
+
+			return value;
+		}
+
 	
 		NameCodeType namecode_;
 	};
 
 	// Type specialization	
 
-	//============= uint128 =================
-	_CONST_EXPR_ std::string to_string_uint128(uint128 name_code);
-	_CONST_EXPR_ uint128 uint128_to_name_code(const char* str);
-	
+	// ---------------- uint128 -----------------------
 	template<>
-	static inline std::string long_name<uint128>::to_string(uint128 name) {
-		return to_string_uint128(name);
-	}
-
-	template<>
-	static inline uint128 long_name<uint128>::to_name_code(const char* str) {
-		return uint128_to_name_code(str);
-	}
-
-	/*template<>
-	class long_name<uint128> {
-	public:
+	inline constexpr long_name<uint128>::size_type long_name<uint128>::char_size() { return 6; }
 
 
-		static inline uint128 to_name_code(const char* str) {
-			return uint128_to_name_code(str);
+	typedef std::unordered_map<char, long_name<uint128>::char_code_type> reverse_code_map_type_u128;
+
+	//  Construction of 128 bits: c0 c1 c2 ... c20 00
+	//  Each char take 6 bits
+	const static char CODE_MAP_U128[] = {
+		'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+		'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
+		'.', '#', '@', '$', '%', '_' , '-'
+	};
+
+	static inline reverse_code_map_type_u128 consturct_reverse_code_map_u128() {
+		reverse_code_map_type_u128 reverse_code_map;
+		for (long_name<uint128>::char_code_type i = 0; i < sizeof(CODE_MAP_U128); ++i)
+		{
+			reverse_code_map[CODE_MAP_U128[i]] = i;
 		}
-	};*/
+		return reverse_code_map;
+	}
+
+	template<>
+	static long_name<uint128>::char_code_type long_name<uint128>::char_to_code(char chr) {
+		static reverse_code_map_type_u128 reverse_code_map = consturct_reverse_code_map_u128();
+		return reverse_code_map.at(chr);
+	}
+
+	template<>
+	static char long_name<uint128>::code_to_char(long_name<uint128>::char_code_type code) {
+		assert(code >= 0 && code < sizeof(CODE_MAP_U128));
+		return CODE_MAP_U128[code];
+	}
+
+
 }
--- foundation/utilities/include/string_utilities.hpp
+++ foundation/utilities/include/string_utilities.hpp
@@ -3,12 +3,12 @@
 
 namespace xmax {
 		
-	static _CONST_EXPR_ std::string&& name_to_string(Xmaxplatform::Basetypes::name::name_code_type name)
+	static std::string&& name_to_string(Xmaxplatform::Basetypes::name::name_code_type name)
 	{
 		return Xmaxplatform::Basetypes::name::to_string(name);
 	}
 
-	static _CONST_EXPR_ Xmaxplatform::Basetypes::name::name_code_type string_to_name(const char* str)
+	static Xmaxplatform::Basetypes::name::name_code_type string_to_name(const char* str)
 	{
 		return Xmaxplatform::Basetypes::name::to_name_code(str);
 	}
