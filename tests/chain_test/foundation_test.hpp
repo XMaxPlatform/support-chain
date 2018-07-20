69575c0bb730660239ec09d43243ddf81fffd876
williamsg <williamsg@163.com>
2018-07-20 17:56:27 +0800

    Supplement some complex cases for the longname test case

--- tests/chain_test/foundation_test.hpp
+++ tests/chain_test/foundation_test.hpp
@@ -2,6 +2,7 @@
 
 #include "basetypes.hpp"
 #include "string_utilities.hpp"
+#include <array>
 
 
 
@@ -10,9 +11,22 @@ using namespace Xmaxplatform::Basetypes;
 BOOST_AUTO_TEST_SUITE(foundation_test_suite)
 
 BOOST_AUTO_TEST_CASE(test_long_name) {
-	name code = xmax::string_to_name("erc721totalsupply");
-	std::string str = xmax::name_to_string(code);
-	BOOST_CHECK(str.compare("erc721totalsupply") == 0);
+	
+	std::array<std::string, 3> test_strs{
+		"erc721totalsupply",
+		"xmxoffcial@abcd.com",
+		"xmax_message_test"
+	};
+	
+	std::array<name, test_strs.size()> codes;
+
+	for (size_t i = 0; i < test_strs.size(); i++)
+	{
+		name code = xmax::string_to_name(test_strs[i].c_str());
+		std::string str = xmax::name_to_string(code);
+		BOOST_CHECK(str.compare(test_strs[i]) == 0);
+	}
+
 }
 
 
