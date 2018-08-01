bac8dfde7759386909ab454af5b34730e5ba8332
fengzi <liuxingzdh@163.com>
2018-08-01 17:21:21 +0800

    exception.

--- foundation/basetypes/include/xmaxtypes.hpp
+++ foundation/basetypes/include/xmaxtypes.hpp
@@ -4,3 +4,35 @@
 #include <publickey.hpp>
 #include <asset.hpp>
 #include <gentypes.hpp>
+
+
+
+namespace Xmaxplatform {
+namespace Basetypes {
+
+
+	inline bool operator== (const account_permission& lhs, const account_permission& rhs) {
+		return std::tie(lhs.account, lhs.authority) == std::tie(rhs.account, rhs.authority);
+	}
+
+	inline bool operator!= (const account_permission& lhs, const account_permission& rhs) {
+		return std::tie(lhs.account, lhs.authority) != std::tie(rhs.account, rhs.authority);
+	}
+
+	inline bool operator< (const account_permission& lhs, const account_permission& rhs) {
+		return std::tie(lhs.account, lhs.authority) < std::tie(rhs.account, rhs.authority);
+	}
+
+	inline bool operator<= (const account_permission& lhs, const account_permission& rhs) {
+		return std::tie(lhs.account, lhs.authority) <= std::tie(rhs.account, rhs.authority);
+	}
+
+	inline bool operator> (const account_permission& lhs, const account_permission& rhs) {
+		return std::tie(lhs.account, lhs.authority) > std::tie(rhs.account, rhs.authority);
+	}
+
+	inline bool operator>= (const account_permission& lhs, const account_permission& rhs) {
+		return std::tie(lhs.account, lhs.authority) >= std::tie(rhs.account, rhs.authority);
+	}
+}
+}
