9642e4a817bfba958a76a6ca9381c78dcd0fc3d3
williamsg <williamsg@163.com>
2018-08-09 17:38:04 +0800

    Add name2str option implementation to client_tools

--- tools/client_tools/main.cpp
+++ tools/client_tools/main.cpp
@@ -1,6 +1,10 @@
 #include <boost/program_options/variables_map.hpp>
 #include <boost/program_options/parsers.hpp>
+#include <boost/multiprecision/cpp_int.hpp>
 #include <iostream>
+#include "basetypes.hpp"
+#include "string_utilities.hpp"
+
 
 using namespace std;
 
@@ -8,11 +12,13 @@ using namespace std;
 
 
 namespace bpo = boost::program_options;
+using namespace Xmaxplatform::Basetypes;
 
 namespace {
 	void InitProgramOptions(bpo::options_description& options) {
 		options.add_options()
-			("help,h", "Print this help message and exit.");
+			("help,h", "Print this help message and exit.")
+			("name2str", bpo::value<name::name_code_type>(), "Convert name type to string");
 	}
 
 	void ParseProgramOptions(int argc, char** argv, bpo::options_description& options, bpo::variables_map& vars_map) {
@@ -32,9 +38,15 @@ int main(int argc, char** argv)
 
 	if (vars_map.count("help")) {
 		cout << options << std::endl;
-		return false;
+		return 0;
 	}
 
+	if (vars_map.count("name2str"))
+	{
+		auto name_code = vars_map.at("name2str").as<name::name_code_type>();		
+		cout << xmax::name_to_string(name_code) << endl;
+		return 0;
+	}
 
 
 	cout << options << std::endl;
