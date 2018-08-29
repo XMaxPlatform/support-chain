ed55adb378e5013c722a418eb8b9a3c459f79b19
fengzi <liuxingzdh@163.com>
2018-08-29 15:34:32 +0800

    rename cash block.

--- blockchain/chain/cash/cash_block.cpp
+++ blockchain/chain/cash/cash_block.cpp
@@ -3,18 +3,18 @@
 *  @copyright defined in xmax/LICENSE
 */
 #pragma once
-#include <coin/coin_block.hpp>
+#include <cash/cash_block.hpp>
 #include <chain_utils.hpp>
 
 
 namespace Xmaxplatform {
 namespace Chain {
-	xmax_type_summary coin_block_header::digest()const
+	xmax_type_summary cash_block_header::digest()const
 	{
 		return xmax_type_summary::hash(*this);
 	}
 
-	xmax_type_block_num coin_block_header::block_num() const
+	xmax_type_block_num cash_block_header::block_num() const
 	{
 		return utils::num_from_id(previous) + 1;
 	}
--- blockchain/chain/include/cash/cash_block.hpp
+++ blockchain/chain/include/cash/cash_block.hpp
@@ -3,19 +3,19 @@
 *  @copyright defined in xmax/LICENSE
 */
 #pragma once
-#include <coin/coin_types.hpp>
+#include <cash/cash_types.hpp>
 
 
 namespace Xmaxplatform {
 namespace Chain {
 
-	struct coin_block_header
+	struct cash_block_header
 	{
 		xmax_type_summary			digest() const;
 		xmax_type_block_num			block_num() const;
 
 		xmax_type_block_id		previous;
-		chain_timestamp			timestamp;
+		chain_timestamp			timestamp;   
 		account_address			builder;
 	};
 
@@ -23,4 +23,4 @@ namespace Chain {
 }
 
 
-FC_REFLECT(Xmaxplatform::Chain::coin_block_header, (previous)(timestamp)(builder))
+FC_REFLECT(Xmaxplatform::Chain::cash_block_header, (previous)(timestamp)(builder))
