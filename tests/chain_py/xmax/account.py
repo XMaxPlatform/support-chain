118351349258f2cd9611bcb88cbab1dccc894c9e
fengzi <liuxingzdh@163.com>
2018-09-20 16:30:22 +0800

    account generate logic.

--- tests/chain_py/xmax/account.py
+++ tests/chain_py/xmax/account.py
@@ -40,3 +40,25 @@ SIMPLE_NEW_ACCOUNT_JSON = '\
 		"deposit": @{deposit}\n\
 	}\n\
 }'
+
+
+
+def newAccountJson(creatorName, accName, deposit, ownerKey, activeKey):
+
+	stream = SIMPLE_NEW_ACCOUNT_JSON.replace(NEWACC_TRXACC_NAME, utils.quoteValue(creatorName))
+	stream = stream.replace(NEWACC_CREATOR_NAME, utils.quoteValue(creatorName))
+
+	stream = stream.replace(NEWACC_ACCOUNT_NAME, utils.quoteValue(accName))
+
+	stream = stream.replace(NEWACC_OWNER_KEY, utils.quoteValue(ownerKey))
+	stream = stream.replace(NEWACC_ACTIVE_KEY, utils.quoteValue(activeKey))
+	stream = stream.replace(NEWACC_DEPOSIT_NAME, utils.quoteValue(str(deposit)))
+	return stream
+
+GETACC_ACCOUNT_NAME = '@{account}'
+
+GETACC_JSON = '{"account_name":@{account}}'
+
+def getAccountJson(accName):
+	stream = GETACC_JSON.replace(GETACC_ACCOUNT_NAME, utils.quoteValue(accName))
+	return stream
